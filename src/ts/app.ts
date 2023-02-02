71fd654469b3faf1b93dcbfc9d66201f75ebe32a
Niklas Mårdby <niklas.mardby@zocom.se>
2023-02-02 12:33:19 +0100

    Break out interface

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,21 +1,5 @@
-interface SWPeople {
-    name: string,
-    height: string,
-    mass: string,
-    hair_color: string,
-    skin_color: string,
-    eye_color: string,
-    birth_year: string,
-    gender: string,
-    homeworld: string,
-    films: string[],
-    species: string[],
-    vehicles: string[],
-    starships: string[],
-    created: string,
-    edited: string,
-    url: string
-}
+import SWPeople from './SWPeople';
+
 const url = "https://swapi.dev/api/people";
 
 // interface Person2 { [key: string]: string | string[]; }
